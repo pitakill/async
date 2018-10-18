ff28e4a4210b90872b355e271bb678dca3de6775
Lepoldo Caballero <git@pitakill.net>
2018-10-17 21:11:05 -0500

    Simple generator

--- generator.js
+++ generator.js
@@ -8,8 +8,22 @@ function *NaturalNumbers() {
 
 const numbers = NaturalNumbers()
 
-console.log(numbers.next().value)
-console.log(numbers.next().value)
-console.log(numbers.next().value)
-console.log(numbers.next().value)
-console.log(numbers.next().value)
+console.log(numbers.next())
+console.log(numbers.next())
+console.log(numbers.next())
+console.log(numbers.next())
+console.log(numbers.next())
+console.log(numbers.next())
+
+function *GeneratorHolaMundo() {
+  yield "hola"
+  yield "mundo"
+}
+
+const holamundoIterator = GeneratorHolaMundo()
+
+while (true) {
+  const {value, done} = holamundoIterator.next()
+  if (done) return
+  console.log(value)
+}
